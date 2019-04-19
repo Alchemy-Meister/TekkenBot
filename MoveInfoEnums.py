f7e6883fb4a35c0dbaf3e8934255b34480521059
Alchemy-Meister <jesusesma@gmail.com>
2019-04-19 16:38:39 +0200

    Formated MoveInfoEnums to 80 chars per line, added DLC and Story Mode characters codes; Stage ands Main Menu IDs.

--- MoveInfoEnums.py
+++ MoveInfoEnums.py
@@ -1,27 +1,37 @@
 from enum import Enum
 
 class AttackType(Enum):
-    ANTIAIR_ONLY = 11 #Doesn't hit characters on the ground? Very rare, appears on Alisa's chainsaw stance f+2
-    THROW = 10  #this is only the attack type *during* the throw animation
-    LOW_UNBLOCKABLE = 9 #Yoshimitsu's 10 hit combo 2 has one
-    HIGH_UNBLOCKABLE = 8  #Akuma's focus attack
+    """
+
+    """
+    # Doesn't hit characters on the ground?
+    # Very rare, appears on Alisa's chainsaw stance f+2
+    ANTIAIR_ONLY = 11
+    THROW = 10  # This is only the attack type *during* the throw animation
+    LOW_UNBLOCKABLE = 9 # Yoshimitsu's 10 hit combo 2 has one
+    HIGH_UNBLOCKABLE = 8  # Akuma's focus attack
     MID_UNBLOCKABLE = 7
-    #UNKNOWN_6 = 6 #????? may not exist
+    # UNKNOWN_6 = 6 # ????? may not exist
     HIGH = 5
     SMID = 4
-    PROJ = 3 #Special mids that can't be parried. Unknown if/what other properties they share.
+    # Special mids that can't be parried.
+    # Unknown if/what other properties they share.
+    PROJ = 3
     MID = 2
     LOW = 1
-    NA = 0 #This move is not an attack
+    NA = 0 # This move is not an attack
 
 
 class SimpleMoveStates(Enum):
+    """
+
+    """
     UNINITIALIZED = 0
 
     STANDING_FORWARD = 1
     STANDING_BACK = 2
     STANDING = 3
-    STEVE = 4 #steve?
+    STEVE = 4 # steve?
 
 
 
@@ -29,7 +39,7 @@ class SimpleMoveStates(Enum):
     CROUCH_BACK = 6
     CROUCH = 7
 
-    UNKNOWN_TYPE_9 = 9 #seen on Ling
+    UNKNOWN_TYPE_9 = 9 # Seen on Ling
 
     GROUND_FACEUP = 12
     GROUND_FACEDOWN = 13
@@ -37,106 +47,136 @@ class SimpleMoveStates(Enum):
     JUGGLED = 14
     KNOCKDOWN = 15
 
-    #THE UNDERSTANDING OF THE FOLLOWING VALUES IS NOT COMPLETE
+    # THE UNDERSTANDING OF THE FOLLOWING VALUES IS NOT COMPLETE
 
     OFF_AXIS_GETUP = 8
 
-    UNKNOWN_10 = 10 #Yoshimitsu
+    UNKNOWN_10 = 10 # Yoshimitsu
     UNKNOWN_GETUP_11 = 11
 
     WALL_SPLAT_18 = 18
     WALL_SPLAT_19 = 19
     TECH_ROLL_OR_FLOOR_BREAK = 20
 
-    UNKNOWN_23 = 23 #Kuma
+    UNKNOWN_23 = 23 # Kuma
 
-    AIRBORNE_24 = 24 #Yoshimitsu
+    AIRBORNE_24 = 24 # Yoshimitsu
     AIRBORNE = 25
-    AIRBORNE_26 = 26 #Eliza. Chloe
-    FLY = 27 #Devil Jin 3+4
+    AIRBORNE_26 = 26 # Eliza, Chloe
+    FLY = 27 # Devil Jin 3+4
 
 
 
 
-class ComplexMoveStates(Enum):  #These are tracking states>
-    F_MINUS = 0 # this doubles as the nothing state and an attack_starting state. #occurs on kazuya's hellsweep
+class ComplexMoveStates(Enum):
+    """
+    These are tracking states>
+    """
+    # This doubles as the nothing state and an attack_starting state.
+    # Occurs on kazuya's hellsweep
+    F_MINUS = 0
 
-    S_PLUS = 1 #homing
-    S = 2 #homing, often with screw, seems to more often end up slightly off-axis?
-    A = 3 #this move 'realigns' if you pause before throwing it out
-    UN04 = 4 # extremely rare, eliza ff+4, 2 has this
-    C_MINUS = 5 # realigns either slightly worse or slightly better than C, hard to tell
-    A_PLUS = 6 #realigns very well #Alisa's b+2, 1 has this, extremely rare
-    C = 7 #this realigns worse than 'A'
+    S_PLUS = 1 # Homing
+    # Homing, often with screw, seems to more often end up slightly off-axis?
+    S = 2
+    A = 3 # This move 'realigns' if you pause before throwing it out
+    UN04 = 4 # Extremely rare, eliza ff+4, 2 has this
+    # Realigns either slightly worse or slightly better than C, hard to tell
+    C_MINUS = 5
+    A_PLUS = 6 # Realigns very well. Alisa's b+2, 1 has this, extremely rare
+    C = 7 # This realigns worse than 'A'
 
-    END1 = 10 #after startup  ###Kazuya's ff+3 doesn't have a startup or attack ending flag, it's just 0 the whole way through ???  ###Lili's d/b+4 doesn't have it after being blocked
+    # After startup
+    #   Kazuya's ff+3 doesn't have a startup or attack ending flag,
+    #       it's just 0 the whole way through ???
+    #   Lili's d/b+4 doesn't have it after being blocked
+    END1 = 10
     BLOCK = 11
-    WALK = 12 #applies to dashing and walking
-    SIDEROLL_GETUP = 13 #only happens after side rolling???
+    WALK = 12 # Applies to dashing and walking
+    SIDEROLL_GETUP = 13 # Only happens after side rolling???
     SIDEROLL_STAYDOWN = 14
-    SS = 15 #sidestep left or right, also applies to juggle techs
+    SS = 15 # Sidestep left or right, also applies to juggle techs
 
-
-    RECOVERING = 16 #happens after you stop walking forward or backward, jumping, getting hit, going into a stance, and some other places
+    # Happens after you stop walking forward or backward, jumping, getting hit,
+    # going into a stance, and some other places
+    RECOVERING = 16
     UN17 = 17  # f+4 with Ling
-    UN18 = 18 #King's 1+2+3+4 ki charge
+    UN18 = 18 # King's 1+2+3+4 ki charge
 
-    UN20 = 20 #Dragunov's d+3+4 ground stomp
+    UN20 = 20 # Dragunov's d+3+4 ground stomp
 
-    UN22 = 22 #Eddy move
-    UN23 = 23 #Steve 3+4, 1
+    UN22 = 22 # Eddy move
+    UN23 = 23 # Steve 3+4, 1
 
-    SW = 28 #sidewalk left or right
+    SW = 28 # Sidewalk left or right
 
 
-    UNKN = 999999 #used to indicate a non standard tracking move
+    UNKN = 999999 # Used to indicate a non standard tracking move
 
 class ThrowTechs(Enum):
     NONE = 0
-    TE1 = 1 #both 1 and 2 seem to sometimes include normal throws that can be broken with either
+    # Both, 1 and 2 seem to sometimes include normal throws that can be broken
+    # with either
+    TE1 = 1
     TE2 = 2
     TE1_2 = 3
 
 class StunStates(Enum):
     NONE = 0
-    UNKNOWN_2 = 2 #Lili BT/Jumping/Kicks?
+    UNKNOWN_2 = 2 # Lili BT/Jumping/Kicks?
     BLOCK = 0x01000100
     GETTING_HIT = 0x100
     DOING_THE_HITTING = 0x10000
-    BEING_PUNISHED = 0x10100 #One frame at the begining of a punish #Also appears during simultaneous couterhits
+    # One frame at the begining of a punish #Also appears during simultaneous
+    # couterhits
+    BEING_PUNISHED = 0x10100
 
-    BLOCK_NO_HIT = 0x1000000 #law's UF+4, sometimes???? Proximity guard maybe?
+    BLOCK_NO_HIT = 0x1000000 # Law's UF+4, sometimes???? Proximity guard maybe?
 
 class RawCancelStates(Enum):
-    STUCK = 0 #Pressing buttons doesn't do anything
-    UNKNOWN_1 = 1 #1 frames occurs during Alisa's u/f 1+2 command grab, also occurs during asuka's parry escapes
+    STUCK = 0 # Pressing buttons doesn't do anything
+    # 1 frame occurs during Alisa's u/f 1+2 command grab, also occurs during
+    # Asuka's parry escapes
+    UNKNOWN_1 = 1
     CANCELABLE = 0x00010000
-    BUFFERABLE = 0x01010000 #coming out of attack for sure, probably block and hit stun too?
-    UNKNOWN_2 = 2 #Alisa's d+3 and chainsaw stance moves cause this, maybe it's a conditional buffer?  Also triggers during normal throws
-    MOVE_ENDING_1 = 0x00010001  # ??? 3 frames at the end of cancel window??? alisa d+2
-    MOVE_ENDING_2 = 0x00010002 #??? 1 frames near the end (or at the end?) of cancelable moves
-    #Theory: 1 and 2 refer to 'parryable' states, these include the active frames of moves and the throw tech windows of moves
-    # the next bit is the cancelable/not cancelable bit and finally there's a 'is being buffered' bit
-    #EDIT: Doesn't seem to be parryyable state. Mostly correspond to active frames, but not entirely.
+    # Coming out of attack for sure, probably block and hit stun too?
+    BUFFERABLE = 0x01010000
+    # Alisa's d+3 and chainsaw stance moves cause this, maybe it's a conditional
+    # buffer?  Also triggers during normal throws
+    UNKNOWN_2 = 2
+    # ??? 3 frames at the end of cancel window??? Alisa d+2
+    MOVE_ENDING_1 = 0x00010001
+    #??? 1 frame near the end (or at the end?) of cancelable moves
+    MOVE_ENDING_2 = 0x00010002
+    # Theory: 1 and 2 refer to 'parryable' states, these include the active
+    #   frames of moves and the throw tech windows of moves
+    # the next bit is the cancelable/not cancelable bit and finally there's a
+    #   'is being buffered' bit
+    # EDIT: Doesn't seem to be parryyable state. Mostly correspond to active
+    # frames, but not entirely.
 
 class CancelStatesBitmask(Enum):
-    CANCELABLE =  0x00010000
-    BUFFERABLE =  0x01000000
+    CANCELABLE = 0x00010000
+    BUFFERABLE = 0x01000000
     PARRYABLE_1 = 0x00000001
     PARRYABLE_2 = 0x00000002
 
-
-#Note that this information resides on the player BEING hit not the player doing the hitting. Also note that there's no counter hit state for side or back attacks.
 class HitOutcome(Enum):
+    """
+    # Note that this information resides on the player BEING hit not the player
+    # doing the hitting. Also note that there's no counter hit state for side or
+    # back attacks.
+    """
     NONE = 0
     BLOCKED_STANDING = 1
     BLOCKED_CROUCHING = 2
     JUGGLE = 3
     SCREW = 4
-    UNKNOWN_SCREW_5 = 5 #Xiaoyu's sample combo 3 ends with this, off-axis or right side maybe?
-    UNKNOWN_6 = 6 #May not exist???
-    UNKNOWN_SCREW_7 = 7 #Xiaoy's sample combo 3 includes this
-    GROUNDED_FACE_DOWN= 8
+    # Xiaoyu's sample combo 3 ends with this, off-axis or right side maybe?
+    UNKNOWN_SCREW_5 = 5
+    UNKNOWN_6 = 6 # May not exist???
+    UNKNOWN_SCREW_7 = 7 # Xiaoyu's sample combo 3 includes this
+    GROUNDED_FACE_DOWN = 8
     GROUNDED_FACE_UP = 9
     COUNTER_HIT_STANDING = 10
     COUNTER_HIT_CROUCHING = 11
@@ -151,8 +191,8 @@ class HitOutcome(Enum):
 
 
 class JumpFlagBitmask(Enum):
-    #GROUND = 0x800000
-    #LANDING_OR_STANDING = 0x810000
+    # GROUND = 0x800000
+    # LANDING_OR_STANDING = 0x810000
     JUMP = 0x820000
 
 class InputDirectionCodes(Enum):
@@ -218,7 +258,7 @@ class CharacterCodes(Enum):
     JOSIE = 24
     GIGAS = 25
     KAZUMI = 26
-    DEVIL_KAZUMI = 27 #not selectable
+    DEVIL_KAZUMI = 27 # Not selectable
     NINA = 28
     MASTER_RAVEN = 29
     LEE = 30
@@ -227,12 +267,79 @@ class CharacterCodes(Enum):
     KUMA = 33
     PANDA = 34
     EDDY = 35
-    ELIZA = 36
+    ELIZA = 36 # DLC
     MIGUEL = 37
+    TEKKEN_FORCE = 38 # Not selectable
+    KID_KAZUYA = 39 # Not selectable
+    JACK_4 = 40 # Not selectable
+    YOUNG_HEIHACHI = 41 # Not selectable
+    GEESE = 43 # DLC
+    TRAINING_DUMMY = 42 # Not selectable
+    NOCTIS = 44 # DLC
+    LEI = 46 # DLC
+    MARDUK = 47 # DLC
+    NEGAN = 50 # DLC
 
-    NOT_YET_LOADED = 71 #value when a match starts for (??) frames until char_id loads
+    # Value when a match starts for (??) frames until char_id loads
+    NOT_YET_LOADED = 71
 
-    NO_SELECTION = 255 #value if cursor is not shown
+    NO_SELECTION = 255 # Value if cursor is not shown
+
+class StageIDs(Enum):
+    """
+
+    """
+    MISHIMA_DOJO = 0
+    FORGOTTEN_REALM = 1
+    JUNGLE_OUTPOST = 2
+    ARCTIC_SNOWFALL = 3
+    TWILIGHT_CONFLICT = 4
+    DRAGON_NEST = 5
+    SOUQ = 6
+    DEVILS_PIT = 7
+    MISHIMA_BUILDING = 8
+    ABANDONED_TEMPLE = 9
+    DUOMO_DI_SIRIO = 30
+    ARENA = 31
+    G_CORP_HELIPAD = 32
+    G_CORP_HELIPAD_NIGHT = 33
+    BRIMSTONE_AND_FIRE = 35
+    PRECIPICE_OF_FATE = 36
+    VIOLET_SYSTEMS = 37
+    KINDER_GYM = 39
+    INFINITE_AZURE = 40
+    GEOMETRIC_PLANE = 41
+    WARM_UP = 42 # Not selectable
+    HOWARD_ESTATE = 51
+    HAMMERHEAD = 52
+    JUNGLE_OUTPOST_2 = 53
+    TWILIGHT_CONFLICT_2 = 54
+    INFINITE_AZURE_2 = 55
+    LAST_DAY_ON_EARTH = 56 # DLC
+
+class MainMenuIDs(Enum):
+    """
+
+    """
+    STORY = 0
+    ONLINE = 1
+    OFFLINE = 2
+    CUSTOMIZATION = 3
+    GALLERY = 5
+    OPTIONS = 6
+    PLAYER_INFORMATION = 7
+    STORE = 8
+    ULTIMATE_TEKKEN_BOWL = 10
+    QUIT = 11
+
+class OfflineMainMenuIDs(Enum):
+    """
+
+    """
+    ARCADE_BATTLE = 0
+    TREASURE_BATTLE = 1
+    VS_BATTLE = 2
+    PRACTICE = 3
 
 class UniversalAnimationCodes(Enum):
     NEUTRAL = 32769
