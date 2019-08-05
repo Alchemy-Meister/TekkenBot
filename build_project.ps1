0a99539d1dfa290ef1ae4ffac8b4a3848fff09c0
Alchemy-Meister <jesusesma@gmail.com>
2019-08-05 05:26:35 +0200

    exclude settings.ini in the building script, ready for release 0.1.5

--- build_project.ps1
+++ build_project.ps1
@@ -1,6 +1,7 @@
 <# get pyinstaller if you haven't already! #>
 <# pip install pyinstaller #>
 pyinstaller TekkenBotPrime.spec
-xcopy /s /Y /i data dist\data
+Remove-Item -Path 'dist\data' -Recurse -Force
+Copy-Item -Path 'data' -Destination 'dist\data' -Recurse -Force -Exclude 'settings.ini'
 Compress-Archive -Force -Path dist\data,dist\TekkenBotPrime.exe -DestinationPath dist\TekkenBotPrime_vX.X.X.zip
 pause
