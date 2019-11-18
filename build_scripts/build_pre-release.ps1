0ce3ed2407aacc022af520e838d35e8d9c3a8e77
Alchemy-Meister <jesusesma@gmail.com>
2019-11-18 17:18:50 +0100

    fixed a bug with copy-item not overwriting folder

--- build_scripts/build_pre-release.ps1
+++ build_scripts/build_pre-release.ps1
@@ -1,5 +1,6 @@
 Set-Location $(Split-Path $PSScriptRoot -Parent)
 & .\build_scripts\build_project.ps1
 $version=$(python ./build_scripts/increase_version.py)
+Copy-Item -Path 'data' -Destination 'dist\data' -Recurse -Force -Exclude 'settings.ini'
 $destination_path='dist\TekkenBotPrime_v{0}.zip' -f $version
 Compress-Archive -Force -Path dist\data,dist\TekkenBotPrime.exe -DestinationPath $destination_path
--- build_scripts/build_project.ps1
+++ build_scripts/build_project.ps1
@@ -1,4 +1,8 @@
 Set-Location $(Split-Path $PSScriptRoot -Parent)
 Remove-Item -Path 'dist' -Recurse -Force
 pyinstaller TekkenBotPrime.spec
-Copy-Item -Path 'data' -Destination 'dist\data' -Recurse -Force -Exclude 'settings.ini'
+
+if ($MyInvocation.CommandOrigin -eq 'Runspace')
+{
+    Copy-Item -Path 'data' -Destination 'dist\data' -Recurse -Force -Exclude 'settings.ini'
+}
